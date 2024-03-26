e4f5d13f2227a53d6d0a2ff8956bb774a334886e
Nolan Pollack <nolanfpollack@gmail.com>
2024-03-26 00:39:47 -0400

    new stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import ConnectionBox from "@/app/ConnectionBox";
+import DataVisual from "@/app/d3_test";
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen px-40 py-4 h-full">
-        <ConnectionBox />
+        <DataVisual />
+
+        {/*<ConnectionBox />*/}
     </main>
   );
 }
