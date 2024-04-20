3afbcf4b8621c5c57e8b261741cef059eb0bec0f
Nolan Pollack <nolanfpollack@gmail.com>
2024-04-20 00:37:26 -0400

    some nice changes

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,15 @@ const nextConfig = {
                 destination: 'https://9fws378c86.execute-api.us-east-2.amazonaws.com/test/:path*',
             },
         ];
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.nba.com',
+                port: '',
+            }
+        ]
     }
 };
 
--- src/app/RadialTree.tsx
+++ src/app/RadialTree.tsx
@@ -1,11 +1,12 @@
 "use client";
 import * as d3 from "d3";
 import {HierarchyPointLink, HierarchyPointNode} from "d3";
-import data from "./paths_teams_victor_wembanyama.json";
+import data from "./paths_teams_lebron.json";
 import {animated, to, useSpring} from "@react-spring/web";
 import {Zoom} from '@visx/zoom';
 import {TransformMatrix} from "@visx/zoom/lib/types";
-import {useState} from "react";
+import React, {useState} from "react";
+import Image from "next/image";
 
 interface PlayerNode {
     id: number;
@@ -22,20 +23,20 @@ interface TeamNode {
 
 interface Props {
     data: TeamNode;
-    handlePathHover: (team: TeamNode) => void;
+    handlePathHover: (team: TeamNode, event: React.MouseEvent) => void;
     handlePopupClose: () => void;
 }
 
 function Path({link, handlePathHover, handlePopupClose}: {
     link: HierarchyPointLink<TeamNode>,
-    handlePathHover: (team: TeamNode) => void,
+    handlePathHover: (team: TeamNode, event: React.MouseEvent) => void,
     handlePopupClose: () => void
 }) {
     const [{strokeWidth, strokeOpacity}, api] = useSpring(() => ({strokeWidth: 1, strokeOpacity: .3}));
 
-    function onHover() {
+    function onHover(event: React.MouseEvent<SVGPathElement, MouseEvent>) {
         api.start({strokeWidth: 1.75, strokeOpacity: 1});
-        handlePathHover(link.target.data);
+        handlePathHover(link.target.data, event);
     }
 
     function onMouseOut() {
@@ -119,6 +120,7 @@ function teamColor(team: string) {
             return "stroke-blue-600";
         default:
             console.log("Unknown team: " + team);
+            return "slate-300"
     }
 }
 
@@ -131,7 +133,7 @@ function RadialTree({data, handlePathHover, handlePopupClose}: Props) {
     const radius = Math.min(width, width) / 2;
 
     const tree = d3.tree<TeamNode>()
-        .size([5 * Math.PI, radius])
+        .size([2 * Math.PI, radius])
         .separation((a, b) => (a.parent == b.parent ? 1 : 2) / (a.depth ?? 1));
 
     // Sort the tree and apply the layout.
@@ -156,21 +158,21 @@ function RadialTree({data, handlePathHover, handlePopupClose}: Props) {
         return props;
     }
 
-    return <Zoom<SVGSVGElement>
-        width={width}
-        height={height}
-        scaleXMin={1}
-        scaleXMax={8}
-        scaleYMin={1}
-        scaleYMax={8}
-        initialTransformMatrix={initialTransform}>
-        {(zoom) => (
-            <svg viewBox={`${cx} ${cy} ${width} ${height}`}
-                 className="text-md h-full w-auto border-2 touch-none rounded-md"
-                 ref={zoom.containerRef}
-                 onWheel={() => updateTransform(zoom.isDragging, zoom.transformMatrix)}
-                 onMouseDown={() => updateTransform(zoom.isDragging, zoom.transformMatrix)}
-                 onMouseMove={() => updateTransform(zoom.isDragging, zoom.transformMatrix)}
+    // return <Zoom<SVGSVGElement>
+    //     width={width}
+    //     height={height}
+    //     scaleXMin={1}
+    //     scaleXMax={8}
+    //     scaleYMin={1}
+    //     scaleYMax={8}
+    //     initialTransformMatrix={initialTransform}>
+    //     {(zoom) => (
+            return <svg viewBox={`${cx} ${cy} ${width} ${height}`}
+                 className="text-md flex-grow touch-none"
+                 // ref={zoom.containerRef}
+                 // onWheel={() => updateTransform(zoom.isDragging, zoom.transformMatrix)}
+                 // onMouseDown={() => updateTransform(zoom.isDragging, zoom.transformMatrix)}
+                 // onMouseMove={() => updateTransform(zoom.isDragging, zoom.transformMatrix)}
             >
                 <animated.g style={
                     {
@@ -189,13 +191,110 @@ function RadialTree({data, handlePathHover, handlePopupClose}: Props) {
                     </g>
                     <g>
                         {root.descendants().filter((node) => node.depth < numLayers + 1).map((node, i) => (
-                            <circle key={i} r={1} className="fill-slate-300" id={node.data.team_name}
+                            <circle key={i} r={1} className={"fill-stone-400"} id={node.data.team_name}
                                     transform={`rotate(${node.x * 180 / Math.PI - 90}) translate(${node.y},0)`}></circle>
                         ))}
                     </g>
                 </animated.g>
-            </svg>)}
-    </Zoom>
+            </svg>
+    //     )}
+    // </Zoom>
+}
+
+function CanvasRadialTree({data, handlePathHover, handlePopupClose}: Props) {
+    // Create canvas version of the svg
+    // const numLayers = 10;
+    const width = 1000;
+    const height = 1000;
+    const radius = Math.min(width, width) / 2;
+
+    const tree = d3.tree<TeamNode>()
+        .size([5 * Math.PI, radius])
+        .separation((a, b) => (a.parent == b.parent ? 1 : 2) / (a.depth ?? 1));
+
+    // Sort the tree and apply the layout.
+    const root = tree(d3.hierarchy(data, d => d.players?.map(p => p.teams ?? []).flat())
+        .sort((a, b) => d3.ascending(a.data.team_name, b.data.team_name)));
+
+    const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+    function polarToCartesian(distance: number, angle: number) {
+        const x = width / 2 + distance * Math.cos(angle);
+        const y = height / 2 + distance * Math.sin(angle);
+        return {x: x, y: y};
+    }
+
+    React.useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas?.getContext("2d");
+        if (context) {
+            context.clearRect(0, 0, width, height);
+            root.descendants().forEach((node) => {
+                const {x, y} = polarToCartesian(node.y, node.x);
+                context.beginPath();
+                context.arc(x, y, 1, 0, 2 * Math.PI);
+                context.fillStyle = "white";
+                context.fill();
+            })
+            root.links().forEach((link) => {
+                const source = polarToCartesian(link.source.y, link.source.x);
+                const target = polarToCartesian(link.target.y, link.target.x);
+                context.beginPath();
+                context.moveTo(source.x, source.y);
+                context.lineTo(target.x, target.y);
+                context.strokeStyle = teamColor(link.target.data.team_name);
+                context.lineWidth = 1;
+                context.stroke();
+            })
+        }
+    }, [root]);
+
+    return <canvas ref={canvasRef} width={width} height={height}
+                   className="text-md border-2 touch-none rounded-md"></canvas>;
+}
+
+function getHeadshotURL(playerID: number) {
+    return "https://cdn.nba.com/headshots/nba/latest/260x190/" + playerID + ".png";
+}
+
+function TeamInfo({popupPosition, popupData}: { popupPosition: { x: number, y: number }, popupData: TeamNode }) {
+    return <div className={"absolute rounded-md bg-stone-200 p-4 border-black shadow-xl top-2 left-2 divide-y"}
+                style={
+                    {
+                        top: popupPosition.y,
+                        left: popupPosition.x
+                    }
+
+                }>
+        <h1 className="text-xl">{popupData.season + " " + popupData.team_name}</h1>
+        <h2>{}</h2>
+        <ul>
+            {popupData.players?.map((player) =>
+                <li key={player.id}>
+                    {/*<Image src={getHeadshotURL(player.id)} alt="headshot" width={52} height={38}/>*/}
+                    {player.name}
+                </li>)}
+        </ul>
+    </div>
+}
+
+function Title({data_node}: {data_node: PlayerNode}) {
+    return <div className="flex justify-center items-center">
+        {/*<Image src={getHeadshotURL(data_node.id)} alt="headshot" width={260} height={190}/>*/}
+        <form className="flex justify-center py-4 my-4" onSubmit={(e) => e.preventDefault()}>
+                <input
+                    className="w-full text-center font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-yellow-600 text-5xl sm:text-6xl lg:text-8xl caret-stone-200 focus:outline-none"
+                    type={"text"} placeholder={"playuh"} maxLength={24}/>
+
+        </form>
+        {/*<h1*/}
+        {/*    className="py-4 my-4 text-center font-extrabold">*/}
+        {/*    <span*/}
+        {/*        className="bg-gradient-to-r from-purple-600 to-yellow-600 bg-clip-text text-transparent text-5xl sm:text-6xl lg:text-8xl">*/}
+        {/*        {data_node.name}*/}
+        {/*    </span>*/}
+        {/*</h1>*/}
+    </div>
 }
 
 
@@ -205,29 +304,30 @@ export default function DataVisual() {
 
     const [popupOpen, setPopupOpen] = useState(false);
     const [popupData, setPopupData] = useState({} as TeamNode);
-    const [popupPosition, setPopupPosition] = useState([0, 0] as [number, number]);
+    const [popupPosition, setPopupPosition] = useState({x: 0, y: 0});
 
-    function handlePathHover(team: TeamNode) {
+    function handlePathHover(team: TeamNode, event: React.MouseEvent) {
         setPopupOpen(true);
         setPopupData(team);
+        setPopupPosition({x: event.clientX, y: event.clientY});
     }
 
     function handlePopupClose() {
         setPopupOpen(false);
     }
 
-    return <div className="h-full w-auto">
-        <h1
-            className="bg-gradient-to-r from-purple-600 to-yellow-600 bg-clip-text text-transparent text-5xl font-extrabold">
-            {data_node.name}</h1>
+    data_node.teams?.sort((a, b) => a.season.localeCompare(b.season));
+
+
+    return <div className="flex flex-col h-full w-full">
+        <Title data_node={data_node}/>
         <RadialTree data={data_wrapper} handlePathHover={handlePathHover} handlePopupClose={handlePopupClose}/>
-        {popupOpen &&
-            <div className={"absolute rounded-md bg-stone-200 p-4 border-black shadow-xl top-2 left-2 divide-y"}>
-                <h1 className="text-xl">{popupData.season + " " + popupData.team_name}</h1>
-                <ul>
-                    {popupData.players?.map((player) => <li key={player.id}>{player.name}</li>)}
-                </ul>
-            </div>}
+        {/*<div className="flex justify-center h-full">*/}
+        {/*    <CanvasRadialTree data={data_wrapper} handlePathHover={handlePathHover}*/}
+        {/*                      handlePopupClose={handlePopupClose}/>*/}
+        {/*</div>*/}
+
+        {popupOpen && <TeamInfo popupPosition={popupPosition} popupData={popupData}/>}
     </div>
 }
 
