1080d873730aa393517cdfa58d48db484dfbd6d6
Nolan Pollack <nolanfpollack@gmail.com>
2024-05-14 03:00:36 -0400

    Loading wheel component

---
+++ src/app/components/LoadingWheel.tsx
@@ -0,0 +1,10 @@
+export default function LoadingWheel() {
+    return (
+        <div className="lds-ring text-orange-400">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
