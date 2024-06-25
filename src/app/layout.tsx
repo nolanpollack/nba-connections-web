b0cfcf8821ee6c8177d2d363484362e06d779595
Nolan Pollack <nolanfpollack@gmail.com>
2024-06-25 13:55:09 -0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,13 +16,14 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className="h-full dark">
+        <html lang="en" className="dark h-full">
             <body
                 className={
                     inter.className + " h-full bg-stone-300 dark:bg-stone-900"
                 }
             >
                 {children}
+                <Analytics />
             </body>
         </html>
     );
